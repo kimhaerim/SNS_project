e31b8a8dafc8820b757d0fb8c11126e4f9420481
juwon Hwang <wndnjs2037@naver.com>
2022-03-18 22:21:52 +0900

    test commit

---
+++ src/main/java/com/codepresso/team2app/mapper/LikeMapper.java
@@ -0,0 +1,7 @@
+package com.codepresso.team2app.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface LikeMapper {
+}
---
+++ src/main/java/com/codepresso/team2app/mapper/PostMapper.java
@@ -0,0 +1,13 @@
+package com.codepresso.team2app.mapper;
+
+import com.codepresso.team2app.vo.Post;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface PostMapper {
+    Integer save(@Param("post") Post post);
+    List<Post> findAllPost();
+}
